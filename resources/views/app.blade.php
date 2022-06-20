1e87f93be5f4e604a3ae9a5379387dab72a5e815
joserodny <josesibugan@gmail.com>
2022-06-21 01:53:32 +0800

    add meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,10 @@
   <head>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+      <meta property="og:type" content="website">
+      <meta property="og:url" content="https://joserodny.herokuapp.com/">
+      <meta property="og:title" content="Jose rodny Sibugan portfolio">
+      <meta property="og:image" content="https://github.com/joserodny/inertiaproject/blob/main/latestport.png?raw=true">
       <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
       <script src="{{ mix('/js/app.js') }}" defer></script>
       <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
