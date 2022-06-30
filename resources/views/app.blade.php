ab96ac129b26cb07fdb082b21b0f76f6461cfec7
joserodny <josesibugan@gmail.com>
2022-07-01 00:27:02 +0800

    add meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
 
       <meta property="og:type" content="website">
       <meta property="og:url" content="https://joserodny.herokuapp.com/">
-      <meta property="og:title" content="ose rodny Sibugan portfolio">
+      <meta property="og:title" content="jose rodny Sibugan portfolio">
       <meta property="og:image" content="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80">
 
       <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
